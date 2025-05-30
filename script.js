5f6a24aebf0814f6ca7ee3fe201da2b72b86b260
berru-g <g.leberruyer@gmail.com>
2025-05-30 21:44:22 +0200

    debugOKlinkNOW

--- script.js
+++ script.js
@@ -31,6 +31,46 @@ const { data, error } = await supabase.from('pixels').select('*')
 const soldMap = new Set(data?.filter(p => p.is_sold).map(p => p.id))
 
 pixels.forEach((id) => {
+  const pixelData = data.find(p => p.id === id)
+  const div = document.createElement('div')
+  div.className = 'pixel'
+  div.dataset.pixelId = id
+
+  const hasContent = pixelData?.image_url || pixelData?.color || pixelData?.link_url
+
+  if (pixelData?.is_sold || hasContent) {
+    div.classList.add('sold')
+
+    if (pixelData.image_url) {
+      div.style.backgroundImage = `url('${pixelData.image_url}')`
+      div.style.backgroundSize = 'cover'
+      div.style.backgroundPosition = 'center'
+    } else if (pixelData.color) {
+      div.style.backgroundColor = pixelData.color
+    }
+
+    if (pixelData.link_url) {
+      div.addEventListener('click', (e) => {
+        e.stopPropagation()
+        window.open(pixelData.link_url, '_blank')
+      })
+    }
+  }
+
+  if (!pixelData?.is_sold && !hasContent) {
+    div.addEventListener('click', () => {
+      if (!user) return alert('Connecte-toi pour acheter.')
+      selectedPixelId = id
+      formContainer.style.display = 'block'
+      window.scrollTo({ top: formContainer.offsetTop, behavior: 'smooth' })
+    })
+  }
+
+  grid.appendChild(div)
+})
+
+/*
+pixels.forEach((id) => {
   const div = document.createElement('div')
   div.className = 'pixel'
   if (soldMap.has(id)) div.classList.add('sold')
@@ -46,7 +86,7 @@ pixels.forEach((id) => {
 
   grid.appendChild(div)
 })
-
+/*
 // Formulaire + envoi vers Stripe
 form.addEventListener('submit', async (e) => {
   e.preventDefault()
